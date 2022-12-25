53e89219e1f88ebe8be4a4dabc70d091f03d4b3f
Alex O'Brien <alex8obrien@gmail.com>
2022-12-25 20:25:14 +0000

    Added about us page - about.tsx

---
+++ pages/company/about.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import styles from '../../styles/Home.module.css'
+import Footer from '../../components/footer/footer'
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About Us</title>
+        <meta name="description" content="About Us Page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <p>
+            About Us
+          </p>
+        </div>
+      </main>
+      <Footer></Footer>
+    </>
+  )
+}
