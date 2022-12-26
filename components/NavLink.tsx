8b382194563b034e0acc91c44a10d959b2f97bd2
Alex O'Brien <alex8obrien@gmail.com>
2022-12-26 15:51:15 +0000

    Added Navitem and navlinks

---
+++ components/NavLink.tsx
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    name: "About Us",
+    path: "/company/about",
+  },
+  {
+    name: "Downloads",
+    path: "/downloads",
+  },
+  {
+    name: "Docs",
+    path: "/docs",
+  },
+  {
+    name: "Blog",
+    path: "/blog",
+  },
+  {
+    name: "Contact Us",
+    path: "/contact",
+  },
+];
