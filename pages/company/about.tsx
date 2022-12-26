1a9357c0ee54d6838c825a655b0333291ff4270f
Alex O'Brien <alex8obrien@gmail.com>
2022-12-26 23:24:41 +0000

    removed header/footer

--- pages/company/about.tsx
+++ pages/company/about.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
-import Footer from '../../components/Footer'
-import Header from '../../components/Header'
 
 export default function About() {
   return (
