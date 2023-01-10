60d9ad82a10233640d45343f3b2a22675397ff6c
Alex O'Brien <alex8obrien@gmail.com>
2023-01-10 23:30:00 +0000

    Removed useless code

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
