eae2c7bb9e6255ecae00d9d7176e8ce65cec81a4
Alex O'Brien <alex8obrien@gmail.com>
2023-01-10 23:29:03 +0000

    Updated Docs index.tsx

--- pages/docs/index.tsx
+++ pages/docs/index.tsx
@@ -1,8 +1,22 @@
+import Head from "next/head";
+import styles from "../../styles/Home.module.css";
+
 export default function Docs() {
   return (
-    <div>
-      <h1>Docs</h1>
-      <p>Docs page</p>
-    </div>
-  );
+    <>
+      <Head>
+        <title>Documents</title>
+        <meta name="description" content="This is the documentation for the app" />
+        <meta name="viewport" content="width=device-width, initial-scale=device-height" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <p>
+            Docs page
+          </p>
+        </div>
+      </main>
+    </>
+  )
 }
