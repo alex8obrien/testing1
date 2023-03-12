1e0517b3c9be71071d1b24c98e2a5813f92cc1bf
Alex O'Brien <alex8obrien@gmail.com>
2023-03-12 22:54:13 +0000

    Updated Blogs

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,7 +1,22 @@
-export default function Header() {
+import Head from "next/head";
+import styles from "../../styles/Home.module.css";
+
+export default function Blogs() {
   return (
-    <div>
-      <h1>Blog</h1>
-    </div>
-  );
+    <>
+      <Head>
+        <title>Blog</title>
+        <meta name="description" content="This is my blog" />
+        <meta name="viewport" content="width=device-width, initial-scale=device-height" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <p>
+            Blog page
+          </p>
+        </div>
+      </main>
+    </>
+  )
 }
